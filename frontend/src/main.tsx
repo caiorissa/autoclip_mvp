a27e015b939136cac3ac281e23f8a5f367299eb6
Caio Rissa Silveira <135877248+caiorissa@users.noreply.github.com>
2026-09-22 01:09:18 -0300

    pt-BR: remover últimos resíduos em main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import 'dayjs/locale/zh-cn'
 import App from './App.tsx'
 import './index.css'
 
-// 设置dayjs中文
+// Configura o locale do dayjs
 dayjs.locale('zh-cn')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
